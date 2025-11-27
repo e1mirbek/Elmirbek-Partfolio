250c973df004abf9f1eb6e4bd8acb067eda074ad
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-27 19:48:45 +0600

    Файлы для веб-версии

---
+++ lib/web/landing_page_web.dart
@@ -0,0 +1,15 @@
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+class LandingPageWeb extends StatelessWidget {
+  const LandingPageWeb({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
