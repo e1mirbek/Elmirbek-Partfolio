db53c9eac4da477bbba4fb95bc7e5fc2e507c7d1
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-27 19:49:21 +0600

    файлы для mobile версий

---
+++ lib/mobile/landing_page_mobile.dart
@@ -0,0 +1,14 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class LandingPageMobile extends StatelessWidget {
+  const LandingPageMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
