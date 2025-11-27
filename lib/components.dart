606c410cc6358409cde5de9cc9a6543dba5ee22f
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-27 19:47:47 +0600

    components

---
+++ lib/components.dart
@@ -0,0 +1,15 @@
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+class Components extends StatelessWidget {
+  const Components({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
