f7635d563106826764396d7d63e791219073a35c
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-28 21:21:04 +0600

    refactor: добавлена анимированная реакция на hover в TabsWeb

--- lib/components.dart
+++ lib/components.dart
@@ -1,15 +1,50 @@
-
-
-
-
+import "dart:developer" as developer;
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Components extends StatelessWidget {
-  const Components({super.key});
+class TabsWeb extends StatefulWidget {
+  final String title;
+
+  const TabsWeb(this.title, {super.key});
+
+  @override
+  State<TabsWeb> createState() => _TabsWebState();
+}
+
+class _TabsWebState extends State<TabsWeb> {
+  bool isSelectedMouse = false;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() {
+          isSelectedMouse = true;
+        });
+        // developer.log("<<<Мышь зашла на кнопку>>>");
+      },
+      onExit: (_) {
+        setState(() {
+          isSelectedMouse = false;
+        });
+        // developer.log("<<<Мышь ушла с кнопки>>>");
+      },
+      child: AnimatedDefaultTextStyle(
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.elasticIn,
+        style: isSelectedMouse
+            ? GoogleFonts.oswald(
+                fontSize: 25.0,
+                color: Colors.transparent,
+                decoration: TextDecoration.underline,
+                decorationThickness: 1,
+                decorationColor: Colors.black,
+                shadows: [Shadow(offset: Offset(0, -08))],
+              )
+            : GoogleFonts.oswald(fontSize: 23.0, color: Colors.black),
+        child: Text(widget.title),
+      ),
+    );
   }
 }
