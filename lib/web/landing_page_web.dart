9c544c0224472310640af36a4902a5440c598379
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-29 20:42:16 +0600

    UX: улучшена адаптация главного экрана под большие и маленькие дисплеи

--- lib/web/landing_page_web.dart
+++ lib/web/landing_page_web.dart
@@ -1,5 +1,6 @@
 import 'package:elmirbek_abdumanapov/components.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LandingPageWeb extends StatefulWidget {
   const LandingPageWeb({super.key});
@@ -11,6 +12,8 @@ class LandingPageWeb extends StatefulWidget {
 class _LandingPageWebState extends State<LandingPageWeb> {
   @override
   Widget build(BuildContext context) {
+    var heightDevice = MediaQuery.of(context).size.height;
+
     return Scaffold(
       drawer: Drawer(),
       backgroundColor: Colors.white,
@@ -18,7 +21,7 @@ class _LandingPageWebState extends State<LandingPageWeb> {
         backgroundColor: Colors.white,
         title: const Row(
           children: [
-            Spacer(flex: 3,),
+            Spacer(flex: 3),
             TabsWeb("Главная"),
             Spacer(),
             TabsWeb("Проекты"),
@@ -35,7 +38,80 @@ class _LandingPageWebState extends State<LandingPageWeb> {
         elevation: 0.0,
         iconTheme: IconThemeData(color: Colors.black, size: 25.0),
       ),
-      body: SafeArea(child: Center(child: const Text("LandingPageWeb"))),
+      body: SafeArea(
+        child: ListView(
+          // First page
+          children: [
+            Container(
+                padding: EdgeInsets.symmetric(vertical: 40),
+              child: LayoutBuilder(builder: (context, contrains) {
+
+                // Проверка: маленький экран или большой
+                bool isSmall = contrains.maxWidth < 1300;
+
+
+                // Адаптивные размеры логики
+                double avatarSize = isSmall ? 90 : 147;
+                double nameFontSize = isSmall ? 28 : 55;
+                double welcomeFontSize = isSmall ? 12 : 15;
+
+                  return isSmall ?  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                     _buildTextBlock(nameFontSize, welcomeFontSize),
+                      _buildAvatar(avatarSize),
+                    ],
+                  ) :
+                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _buildTextBlock(nameFontSize, welcomeFontSize),
+                     _buildAvatar(avatarSize),
+                    ],
+                  );
+              })
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget _buildAvatar(double size) {
+  return CircleAvatar(
+    radius: size,
+    backgroundColor: Colors.tealAccent,
+    child: CircleAvatar(
+      radius: size - 4,
+      backgroundColor: Colors.black,
+      child: CircleAvatar(
+        radius: size - 7,
+        backgroundImage: AssetImage("assets/images/profile.jpg"),
+      ),
+    ),
+  );
+}
+
+Widget _buildTextBlock(double nameSize, double welcomeSize) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.tealAccent,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: SansBold("Добро пожаловать, я", welcomeSize),
+      ),
+      const SizedBox(height: 15,),
+      SansBold("Элмирбек Абдуманапов", nameSize),
+      const SizedBox(height: 15,),
+    ],
+  );
 }
