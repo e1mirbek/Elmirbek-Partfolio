3632a24e37e2685780466576770637da4d908eba
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-12-01 14:42:36 +0600

    Добавлен список feedbackData с контактной информацией

---
+++ lib/web/date/feedback_data.dart
@@ -0,0 +1,20 @@
+
+
+
+import 'package:flutter/material.dart';
+
+List <Map<String, dynamic>> feedbackData = [
+  {
+    "icon" : Icons.email_outlined,
+    "text" : "elmirgrowth@gmail.com",
+  },
+  {
+    "icon" : Icons.call_outlined,
+    "text" : "+996 701 468 346",
+  },
+  {
+    "icon" : Icons.location_on_outlined,
+    "text" : "20/A, ул.Сатаева, Бишкек",
+  },
+];
+
