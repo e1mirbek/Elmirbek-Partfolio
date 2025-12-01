dd846f122977d12de3bc98e1758aa21768c1efde
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-12-01 14:47:02 +0600

    Добавлена основная LandingPageWeb с адаптивными секциями FirstPage и AboutMe

--- lib/web/landing_page_web.dart
+++ lib/web/landing_page_web.dart
@@ -1,4 +1,5 @@
 import 'package:elmirbek_abdumanapov/components.dart';
+import 'package:elmirbek_abdumanapov/web/widgets/first_page/first_page.dart';
 import 'package:flutter/material.dart';
 
 class LandingPageWeb extends StatefulWidget {
@@ -11,6 +12,18 @@ class LandingPageWeb extends StatefulWidget {
 class _LandingPageWebState extends State<LandingPageWeb> {
   @override
   Widget build(BuildContext context) {
+    final skills = [
+      "Flutter",
+      "Android",
+      "IOS",
+      "UX/UI",
+      "Figma",
+      "Firebase",
+      "MySql",
+      "GitHub",
+      "Git",
+    ];
+
     var heightDevice = MediaQuery.of(context).size.height;
 
     return Scaffold(
@@ -39,140 +52,142 @@ class _LandingPageWebState extends State<LandingPageWeb> {
       ),
       body: SafeArea(
         child: ListView(
-            // First page
+          // First page
           children: [
+            FirstPage(),
+            // Second Page
             Container(
-              height: heightDevice -56,
+              height: heightDevice / 1.5,
+              decoration: BoxDecoration(color: Colors.white),
               child: LayoutBuilder(
                 builder: (context, contrains) {
-                  // Проверка: маленький экран или большой
-                  bool isSmall = contrains.maxWidth < 1200;
+                  // логика проверки ширины
+                  bool isSmallScreenAboutMe = contrains.maxWidth > 1350;
 
-                  // Адаптивные размеры логики
-                  double avatarSize = isSmall ? 90 : 147;
-                  double nameFontSize = isSmall ? 28 : 55;
-                  double welcomeFontSize = isSmall ? 12 : 15;
-                  double professionFontSize = isSmall ? 20 : 30;
+                  // адаптивные размеры
 
-                  return isSmall
-                      ? Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                  double aboutMeInformationFontSize = isSmallScreenAboutMe
+                      ? 40
+                      : 20;
+                  double shortInformationFontSize = isSmallScreenAboutMe
+                      ? 15
+                      : 13;
+                  double imageAboutMeInformationFontSize = isSmallScreenAboutMe
+                      ? heightDevice / 1.0
+                      : heightDevice / 5;
+
+                  return isSmallScreenAboutMe
+                      ? Row(
                           children: [
-                            _buildTextBlock(
-                              nameFontSize,
-                              welcomeFontSize,
-                              professionFontSize,
-                              15,
+                            _buildImageAboutMeInformation(
+                              imageAboutMeInformationFontSize,
                             ),
-                            _buildAvatar(avatarSize),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                _buildTextAboutMe(
+                                  aboutMeInformationFontSize,
+                                  shortInformationFontSize,
+                                ),
+                                Row(
+                                  children: skills
+                                      .map(
+                                        (scill) => Padding(
+                                          padding: const EdgeInsets.only(
+                                            right: 7.0,
+                                          ),
+                                          child: _buildSkillBox(scill),
+                                        ),
+                                      )
+                                      .toList(),
+                                ),
+                              ],
+                            ),
                           ],
                         )
-                      : Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      : Column(
                           children: [
-                            _buildTextBlock(
-                              nameFontSize,
-                              welcomeFontSize,
-                              professionFontSize,
-                              15,
+                            _buildImageAboutMeInformation(
+                              imageAboutMeInformationFontSize,
                             ),
-                            _buildAvatar(avatarSize),
+                            _buildTextAboutMe(
+                              aboutMeInformationFontSize,
+                              shortInformationFontSize,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: skills
+                                  .map(
+                                    (scill) => Padding(
+                                      padding: const EdgeInsets.only(
+                                        right: 7.0,
+                                      ),
+                                      child: _buildSkillBox(scill),
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
                           ],
                         );
                 },
               ),
             ),
-            // Second Page
-            Container(
-              height: heightDevice / 1.5,
-              decoration: BoxDecoration(
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SansBold("Обо мне", 40),
-                  ],
-                ),
-              ),
-            )
           ],
         ),
       ),
     );
   }
-}
 
-Widget _buildAvatar(double size) {
-  return CircleAvatar(
-    radius: size,
-    backgroundColor: Colors.tealAccent,
-    child: CircleAvatar(
-      radius: size - 4,
-      backgroundColor: Colors.black,
-      child: CircleAvatar(
-        radius: size - 7,
-        backgroundImage: AssetImage("assets/images/profile.jpg"),
-      ),
-    ),
-  );
-}
+  // ======== Settings Size Second Page ========
 
-Widget _buildTextBlock(
-  double nameSize,
-  double welcomeSize,
-  double professionSize,
-  feedbackSize,
-) {
-  return Padding(
-    padding: const EdgeInsets.all(15.0),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+  Widget _buildTextAboutMe(
+    double aboutMeInformationSize,
+    double shortInformationSize,
+  ) {
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.tealAccent,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-              bottomRight: Radius.circular(20),
-            ),
-          ),
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: SansBold("Приветствую! Это моя коллекция проектов, созданных с вниманием к деталям.", welcomeSize),
-        ),
+        SansBold("Краткая информация", aboutMeInformationSize),
         const SizedBox(height: 15),
-        SansBold("Элмирбек Абдуманапов", nameSize),
-        const SizedBox(height: 15),
-        Sans("Flutter разработчик | IOS | Android | WEB", professionSize),
-        const SizedBox(height: 15),
-        Row(
-          children: [
-            Icon(Icons.email_outlined),
-            const SizedBox(width: 20),
-            Sans("elmirgrowth@gmail.com", feedbackSize),
-          ],
+        Sans(
+          "Абдуманапов Элмирбек Алтынбекович, родился 23 сентября 2005 года.",
+          shortInformationSize,
         ),
-        const SizedBox(height: 15,),
-        Row(
-          children: [
-            Icon(Icons.call_outlined),
-            const SizedBox(width: 20),
-            Sans("+996 701 468 346", feedbackSize),
-          ],
+        Sans(
+          "Окончил Таласского государственного университета",
+          shortInformationSize,
         ),
-        const SizedBox(height: 15,),
-        Row(
-          children: [
-            Icon(Icons.location_on_outlined),
-            const SizedBox(width: 20),
-            Sans("20/A, ул.Сатаева, Бишкек", feedbackSize),
-          ],
+        Sans("по направлению информационных технологий", shortInformationSize),
+        Sans(
+          "Развитие в сфере IT - Мобильная разработка, базовый backend, версия контроль (Git),",
+          shortInformationSize,
         ),
+        Sans(
+          "работа с базами данных, создание UI, интеграция API",
+          shortInformationSize,
+        ),
+        Sans("решение технических задач.", shortInformationSize),
+        const SizedBox(height: 10),
       ],
-    ),
-  );
+    );
+  }
+
+  Widget _buildImageAboutMeInformation(double imageAboutMeInformation) {
+    return Image.asset(
+      "assets/images/about_me.jpg",
+      height: imageAboutMeInformation,
+    );
+  }
+
+  Widget _buildSkillBox(String skillTitle) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.tealAccent, width: 3.0),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Center(child: Sans(skillTitle, 15.0)),
+    );
+  }
 }
