1599af58c25eac647b0c6c8fb00113e9b2da3c17
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-12-01 14:45:36 +0600

    Добавлен TextBlock с динамическими размерами текста и генерацией FeedbackItem

--- lib/web/widgets/first_page/text_block.dart
+++ lib/web/widgets/first_page/text_block.dart
@@ -1,14 +1,57 @@
-
-
-
-
+import 'package:elmirbek_abdumanapov/components.dart';
+import 'package:elmirbek_abdumanapov/web/date/feedback_data.dart';
+import 'package:elmirbek_abdumanapov/web/widgets/first_page/feedback_item.dart';
 import 'package:flutter/material.dart';
 
 class TextBlock extends StatelessWidget {
-  const TextBlock({super.key});
+  final double welcomeSize;
+  final double nameSize;
+  final double professionSize;
+
+  const TextBlock({
+    super.key,
+    required this.nameSize,
+    required this.welcomeSize,
+    required this.professionSize,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.tealAccent,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: SansBold(
+              "Приветствую! Это моя коллекция проектов, созданных с вниманием к деталям.",
+              welcomeSize,
+            ),
+          ),
+          const SizedBox(height: 15),
+          SansBold("Элмирбек Абдуманапов", nameSize),
+          const SizedBox(height: 15),
+          Sans("Flutter разработчик | IOS | Android | WEB", professionSize),
+          const SizedBox(height: 15),
+          // генерация виджетов из списка
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: feedbackData.map((item) {
+              return FeedbackItem(icon: item["icon"], text: item["text"]);
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
