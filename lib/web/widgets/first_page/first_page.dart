c51bb3c4f4b3452288544da23d1074e4305d8c3a
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-12-01 14:46:07 +0600

    Добавлена FirstPage с адаптивной версткой (Column/Row)

--- lib/web/widgets/first_page/first_page.dart
+++ lib/web/widgets/first_page/first_page.dart
@@ -1,14 +1,54 @@
-
-
-
-
+import 'package:elmirbek_abdumanapov/web/widgets/first_page/feedback_item.dart';
+import 'package:elmirbek_abdumanapov/web/widgets/first_page/text_block.dart';
 import 'package:flutter/material.dart';
+
+import 'avatar_widget.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final heightDevice = MediaQuery.of(context).size.height;
+    return Container(
+      height: heightDevice - 56,
+      child: LayoutBuilder(
+        builder: (context, contrains) {
+
+          // Проверка: маленький экран или большой
+          bool isSmall = contrains.maxWidth < 1200;
+
+          // Адаптивные размеры логики
+          double avatarSize = isSmall ? 90 : 147;
+          double nameFontSize = isSmall ? 28 : 55;
+          double welcomeFontSize = isSmall ? 12 : 15;
+          double professionFontSize = isSmall ? 20 : 30;
+
+          return isSmall
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextBlock(
+                      welcomeSize: welcomeFontSize,
+                      nameSize: nameFontSize,
+                      professionSize: professionFontSize,
+                    ),
+                    AvatarWidget(size: avatarSize),
+                  ],
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextBlock(
+                      welcomeSize: welcomeFontSize,
+                      nameSize: nameFontSize,
+                      professionSize: professionFontSize,
+                    ),
+                    AvatarWidget(size: avatarSize),
+                  ],
+                );
+        },
+      ),
+    );
   }
 }
