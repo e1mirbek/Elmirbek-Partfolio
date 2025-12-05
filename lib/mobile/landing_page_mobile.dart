6f8b698df2fa341550479a614dc2f5175641dbfb
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-12-05 22:21:05 +0600

    feat(ui): добавлен базовый экран LandingPageMobile
    - Создан StatefulWidget с Scaffold и AppBar
    - AppBar прозрачный, с белым фоном и без тени
    - Тело страницы обернуто в SafeArea и отображает заглушку текста
    - Подготовка мобильной версии лендинга для дальнейшего наполнения

--- lib/mobile/landing_page_mobile.dart
+++ lib/mobile/landing_page_mobile.dart
@@ -4,11 +4,28 @@
 
 import 'package:flutter/material.dart';
 
-class LandingPageMobile extends StatelessWidget {
+class LandingPageMobile extends StatefulWidget {
   const LandingPageMobile({super.key});
 
   @override
+  State<LandingPageMobile> createState() => _LandingPageMobileState();
+}
+
+class _LandingPageMobileState extends State<LandingPageMobile> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: const Text("LandingPageMobile"),
+        ),
+      ),
+    );
   }
 }
